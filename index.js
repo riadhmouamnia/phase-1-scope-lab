68e49d8910222f2870855d6597bf0a02aa55a499
Riadh Mouamnia <riadh.mouamnia@gmail.com>
2023-07-21 21:12:16 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+// modifies the customerName variable
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// setBestCustomer()
+function setBestCustomer() {
+  bestCustomer = "not bob";
+  // or we can write this
+  //   window.bestCustomer = "not bob";
+}
+
+// overwriteBestCustomer() overwrites the best customer
+function overwriteBestCustomer(name) {
+  bestCustomer = name;
+}
+
+// changeLeastFavoriteCustomer()
+const leastFavoriteCustomer = "not bob";
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "Trying to reassign the least favorite customer";
+}
